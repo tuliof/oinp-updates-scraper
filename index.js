4153850a2150ade2089a7751b243f883e6000924
Tulio Fernandes <tulio.fernandes@yangaroo.com>
2020-01-07 14:12:54 -0500

    Add date check
    Will only notify posts made on the current day, with the caveat that it
    will notify everytime it runs. Need to persist the last check somewhere.

--- index.js
+++ index.js
@@ -10,14 +10,14 @@ if (process.env.NODE_ENV !== 'production') {
 }
 
 async function main() {
-	console.log('Starting OINP Scraper.');
-	
+	console.log('Starting OINP test.');
+	/*
 	const page = {
 		sectionDate: 'January 2, 2020',
 		firstParagraph: 'The Ontario Immigrant Nominee Program has reached its increased 2019' 
 	};
 	return page;
-	
+	*/
 }
 
 async function getOINPsection() {
@@ -34,29 +34,24 @@ async function getOINPsection() {
 	
     await page.setDefaultNavigationTimeout(0);
 	
-	try {
-		const response = await page.goto(url);
-		await page.waitFor(3000);
+	console.log('Fetching...');
+	const response = await page.goto(url);
+	await page.waitFor(3000);
+
+	console.log("Status code:", response.status());
+
+	const result = await page.evaluate(() => {
+		let body = document.querySelector('body').innerText;
+		let section = document.querySelector('h2#section-0');
+		let sectionMonth = section.innerText;
+		let sectionDate = section.nextElementSibling.innerText;
+		let firstParagraph = section.nextElementSibling.nextElementSibling.nextElementSibling.innerText;
 		
-		console.log("Status code:", response.status());
-		
-		const result = await page.evaluate(() => {
-			let body = document.querySelector('body').innerText;
-			console.log('hey ho!');
-			console.log('Body: ' + body);
-			let section = document.querySelector('h2#section-0');
-			let sectionMonth = section.innerText;
-			let sectionDate = section.nextElementSibling.innerText;
-			let firstParagraph = section.nextElementSibling.nextElementSibling.nextElementSibling.innerText;
-			
-			return { sectionMonth, sectionDate, firstParagraph };
-		});
-		
-		await browser.close();
-		return result;
-	} catch(err) {
-		console.error(err);
-	}
+		return { sectionMonth, sectionDate, firstParagraph };
+	});
+
+	await browser.close();
+	return result;
 }
 
 async function sendSMS(message) {
@@ -69,7 +64,8 @@ async function sendSMS(message) {
 	};
 	request.post('https://rest.nexmo.com/sms/json', { json: credentials }, (err, res, body) => {
 		if (err) { return console.log(err) }
-		console.log(body);
+		const msg = body.messages[0];
+		console.log(`Status: ${msg.status}\nRemaining: ${msg['remaining-balance'].slice(0, 4)}`);
 	});
 }
 
@@ -88,17 +84,20 @@ async function formatMessage(page) {
 	return message;
 }
 
-async function checkDate(sectionDate) {
-	sectionDate = 'Janauary 2, 2020';
-	const lastUpdate = new Date(sectionDate);
+async function checkDate(page) {
+	const lastUpdate = new Date(page.sectionDate).toDateString();
 	
-	if (lastUpdate.toDateString() === new Date().toDateString()) {
-		
+	if (lastUpdate !== new Date().toDateString()) {
+		// If not a post made today, stop.
+		throw new Error("Latest post is older than today.");
 	}
+
+	return page;
 }
 
 main()
+	.then(getOINPsection)
 	.then(checkDate)
-	//.then(getOINPsection)
-	//.then(formatMessage)
-	//.then(sendSMS);
+	.then(formatMessage)
+	.then(sendSMS)
+	.catch(err => console.log(err.message));
